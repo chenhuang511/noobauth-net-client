ebf7cbb0c0f62267187d7ddd59cbb7581bd0cd57
hoangtd <hoangtd@softdreams.vn>
2022-06-24 19:10:25 +0700

    first commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace OAuthNotes.Controllers
+{
+    public class AccountController : Controller
+    {
+        public AccountController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IActionResult Login()
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Challenge("Noob");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return View();
+        }
+
+        public async Task<IActionResult> LogoutIdp()
+        {
+            //First, logout from application
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            //Then logout from Idp
+            string redirectUrl = "https://netclient.vn:5001/account/logout";
+            string idpSignoutUrl = Configuration.GetValue<string>("Noob:LogoutEndpoint") + $"?redirect_url={redirectUrl}";
+            return Redirect(idpSignoutUrl);
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return Content("Access denied");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,112 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+
+namespace OAuthNotes
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var authBuilder = services.AddAuthentication(options =>
+            {
+                // If an authentication cookie is present, use it to get authentication information
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+
+                // If authentication is required, and no cookie is present, use NoobAuth (configured below) to sign in
+                options.DefaultChallengeScheme = "Noob";
+            });
+            authBuilder.AddCookie(); // cookie authentication middleware first
+            authBuilder.AddOAuth("Noob", options =>
+            {
+                // When a user needs to sign in, they will be redirected to the authorize endpoint
+                options.AuthorizationEndpoint = Configuration.GetValue<string>("Noob:AuthorizeEndpoint");
+
+                // NoobAuth server provides the default scope is: "profile email"
+                // scopes when redirecting to the authorization endpoint
+                options.Scope.Add("profile");
+                options.Scope.Add("email");
+
+                // After the user signs in, an authorization code will be sent to a callback
+                // in this app. The OAuth middleware will intercept it
+                options.CallbackPath = new PathString("/authorization-code/callback");
+
+                // The OAuth middleware will send the ClientId, ClientSecret, and the
+                // authorization code to the token endpoint, and get an access token in return
+                options.ClientId = Configuration.GetValue<string>("Noob:ClientId");
+                options.ClientSecret = Configuration.GetValue<string>("Noob:ClientSecret");
+                options.TokenEndpoint = Configuration.GetValue<string>("Noob:TokenEndpoint");
+
+                // Below we call the userinfo endpoint to get information about the user
+                options.UserInformationEndpoint = Configuration.GetValue<string>("Noob:UserinfoEndpoint");
+
+                // Describe how to map the user info we receive to user claims
+                options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "sub");
+                options.ClaimActions.MapJsonKey(ClaimTypes.Name, "preferred_username");
+
+                options.Events = new OAuthEvents
+                {
+                    OnCreatingTicket = async context =>
+                    {
+                        // Get user info from the userinfo endpoint and use it to populate user claims
+                        var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                        var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                        response.EnsureSuccessStatusCode();
+
+                        var user = JObject.Parse(await response.Content.ReadAsStringAsync());
+
+                        context.RunClaimActions(user);
+                    }
+                };
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
